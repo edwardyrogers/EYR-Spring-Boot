5d892b73f3f44e86131cefd90411b072aa716d26
Edward Y. Rogers <edward.y.rogers@gmail.com>
2024-09-12 10:06:01 +0800

    update

--- src/main/kotlin/com/eyr/demo/common/configurations/HazelcastConfiguration.kt
+++ src/main/kotlin/com/eyr/demo/common/configurations/HazelcastConfiguration.kt
@@ -1,10 +1,10 @@
 package com.eyr.demo.common.configurations
 
-import com.hazelcast.client.HazelcastClient
-import com.hazelcast.client.config.ClientConfig
-import com.hazelcast.core.Hazelcast
-import com.hazelcast.core.HazelcastInstance
-import org.springframework.context.annotation.Bean
+//import com.hazelcast.client.HazelcastClient
+//import com.hazelcast.client.config.ClientConfig
+//import com.hazelcast.core.Hazelcast
+//import com.hazelcast.core.HazelcastInstance
+//import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
